e684bf2a9203dcbacdf9c725c8b2836b5a019b31
Turmisoil <107317067+Turmisoil@users.noreply.github.com>
2023-07-11 01:04:25 +0100

    Added UI dependencies and aws-sdk

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,18 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import 'vue-toastification/dist/index.css';
+import Toast from 'vue-toastification';
+
+const options = {}
+
 
 createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     return createApp({ render: () => h(App, props) })
       .mixin({methods: { route }})
+      .use(Toast, options)
       .use(plugin)
       .mount(el)
   },
