c8b08a247ad2b1743af6ef542ef3e1a6bc816a23
Turmisoil <107317067+Turmisoil@users.noreply.github.com>
2023-07-11 01:17:48 +0100

    Added backend logic for pdf upload and database storage

--- app/Http/Controllers/DocumentsController.php
+++ app/Http/Controllers/DocumentsController.php
@@ -2,15 +2,80 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Document;
+use Aws\Textract\TextractClient;
+use Carbon\Carbon;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class DocumentsController extends Controller
 {
+
     public function __invoke(Request $request): RedirectResponse
     {
+
+        $request->validate([
+            'document' => 'required'
+        ]);
+
+        $file = $request->file('document');
+
+        if($file->getClientMimeType() !== 'application/pdf') {
+            return redirect()->back()->withErrors([
+                'document' => 'Please only upload PDF documents.'
+            ]);
+        }
+
+        try {
+
+            $textractClient = new TextractClient([
+                'region' => 'eu-west-2',
+                'version' => 'latest',
+                'credentials' => [
+                    'key' => env('AWS_ACCESS_KEY_ID'),
+                    'secret' => env('AWS_SECRET_ACCESS_KEY')
+                ]
+            ]);
+
+            $startTime = Carbon::now();
+
+            $result = $textractClient->detectDocumentText([
+                'Document' => [
+                    'Bytes' => $file->get()
+                ]
+            ]);
+
+            $blocks = $result->get('Blocks');
+
+            $text = '';
+
+            foreach($blocks as $block) {
+                if ($block['BlockType'] != 'WORD') {
+                    continue;
+                }
+
+                $text .= $block['Text'] . ' ';
+
+            }
+
+            $document = new Document();
+
+            $document->title = $file->getClientOriginalName();
+            $document->text = $text;
+            $document->request_start_time = $startTime;
+            $document->created_at = Carbon::now();
+            $document->updated_at = Carbon::now();
+
+            $document->save();
+
+        } catch (\Throwable $th) {
+            return redirect()->back()->withErrors([
+                'message' => 'Unable to process your request at this time. Please try again later.'
+            ]);
+        }
+
         return redirect()->back()->with([
-            'message' => 'success'
+            'message' => 'Your PDF document was successfully uploaded.'
         ]);
     }
 }
